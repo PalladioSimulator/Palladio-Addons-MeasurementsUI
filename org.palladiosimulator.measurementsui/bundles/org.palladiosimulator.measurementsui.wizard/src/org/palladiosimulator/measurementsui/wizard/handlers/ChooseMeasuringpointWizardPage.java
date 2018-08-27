1557366be6422605009807feb9b2c9bf6da79ec0
Florian <st111292@stud.uni-stuttgart.de>
2018-08-27 16:41:19 +0200

    fixed last problems

---
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizard/handlers/ChooseMeasuringpointWizardPage.java
@@ -0,0 +1,80 @@
+package org.palladiosimulator.measurementsui.wizard.handlers;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.palladiosimulator.simulizar.ui.measuringview.parts.controls.EmptyMpTreeViewer;
+import org.palladiosimulator.simulizar.ui.measuringview.parts.controls.MpTreeViewer;
+import org.palladiosimulator.measurementsui.init.DataApplication;
+
+/**
+ * @author Domas Mikalkinas
+ *
+ */
+
+public class ChooseMeasuringpointWizardPage extends WizardPage {
+    MpTreeViewer emptyMpTreeViewer;
+
+    @Inject
+    MDirtyable dirty;
+
+    @Inject
+    ECommandService commandService;
+
+    public ChooseMeasuringpointWizardPage() {
+        super("Second Page");
+        setTitle("Select Measuring Point");
+        setDescription("description");
+    }
+
+    /**
+     * 
+     * Creates the second page of the wizard, which shows a tabbed
+     * 
+     * @param parent
+     */
+    @Override
+    public void createControl(Composite parent) {
+        Composite container = new Composite(parent, SWT.NONE);
+        FillLayout layout = new FillLayout();
+
+        layout.marginHeight = 0;
+        layout.marginWidth = 0;
+        container.setLayout(layout);
+        setControl(container);
+
+        TabFolder tabFolder = new TabFolder(container, SWT.SINGLE);
+
+        TabItem existingMeasuringTabbedItem = new TabItem(tabFolder, SWT.SINGLE);
+        existingMeasuringTabbedItem.setText("Select existing measuring point");
+
+        Composite existingMPcomposite = new Composite(tabFolder, SWT.SINGLE);
+        existingMPcomposite.setLayout(layout);
+
+        emptyMpTreeViewer = createEmptyMpTreeViewer(existingMPcomposite);
+
+        existingMeasuringTabbedItem.setControl(existingMPcomposite);
+
+        TabItem createMeasuringTabbedItem = new TabItem(tabFolder, SWT.NONE);
+        createMeasuringTabbedItem.setText("Create new measuring point");
+
+        Composite createMPcomposite = new Composite(tabFolder, SWT.SINGLE);
+        createMPcomposite.setLayout(layout);
+
+        createMeasuringTabbedItem.setControl(createMPcomposite);
+
+        setPageComplete(true);
+    }
+
+    private MpTreeViewer createEmptyMpTreeViewer(Composite parent) {
+        return new EmptyMpTreeViewer(parent, dirty, commandService, DataApplication.getInstance());
+    }
+
+}
