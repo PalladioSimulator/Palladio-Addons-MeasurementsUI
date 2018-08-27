57878b754345ca041e60b782d61710ffc61424b4
David Schuetz <david-schuetz@gmx.de>
2018-08-27 19:42:40 +0200

    Added Parsley class javadoc comments

--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.mpview/src/mpview/MonitorRepositorySaveableTreeFormView.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.mpview/src/mpview/MonitorRepositorySaveableTreeFormView.java
@@ -2,5 +2,10 @@ package mpview;
 
 import org.eclipse.emf.parsley.views.SaveableTreeView;
 
+/**
+ * Parsley view which shows all monitors and their respective childs and measuringpoints in a tree view
+ * @author David Schuetz
+ *
+ */
 public class MonitorRepositorySaveableTreeFormView extends SaveableTreeView  {
 }
