311b16e6af84281a86054f8fbbac91391c32faf7
Unknown <lasse.merz@web.de>
2018-09-06 10:53:05 +0200

    Class to create Repositories updated

--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.helloworldmenu/src/helloworldmenu/handlers/SampleHandler.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.helloworldmenu/src/helloworldmenu/handlers/SampleHandler.java
@@ -11,7 +11,7 @@ import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPointRepository
 import org.palladiosimulator.monitorrepository.Monitor;
 import org.palladiosimulator.monitorrepository.MonitorRepository;
 
-import DataCreation.DataMeasuringCreator;
+import DataCreation.DataRepositoryCreator;
 import dataManagement.DataGathering;
 import dataManipulation.ResourceEditor;
 import init.DataApplication;
@@ -25,32 +25,32 @@ public class SampleHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 
-		DataApplication start = DataApplication.getInstance();
-		ResourceEditor editor = new ResourceEditor();
-		DataMeasuringCreator creator = new DataMeasuringCreator();
-		DataGathering gatherer = new DataGathering();
-
-		List<IProject> projects = gatherer.getAllProjectAirdfiles();
-		for(IProject eachProject : projects) {
-			System.out.println("Project Name: "+eachProject.getName());
-			System.out.println("Project Aird Path: "+gatherer.getAirdFile(eachProject));
-		}
-		
-		System.out.println("Testing .monitorrepository return: "+ gatherer.getChosenFile(gatherer.getAllProjectAirdfiles().get(0), "monitorrepository"));
-		
-
-		MonitorRepository monRep = start.getModelAccessor().getMonitorRepository().get(0);
-		MeasuringPointRepository mpRep = start.getModelAccessor().getMeasuringPointRpository().get(0);
-		Monitor mon1 = monRep.getMonitors().get(0);
-		Monitor mon2 = monRep.getMonitors().get(1);
-
-		MeasuringPoint mp1 = mpRep.getMeasuringPoints().get(0);
-		System.out.println("length: "+ mpRep.getMeasuringPoints().size());
-		editor.addMeasuringPoint(mpRep, creator.createResourceURIMeasuringPoint("123gfsda"));
-		System.out.println("length: "+ mpRep.getMeasuringPoints().size());
-		for(MeasuringPoint mp : mpRep.getMeasuringPoints()) {
-			System.out.println(mp.getResourceURIRepresentation());
-		}
+//		DataApplication start = DataApplication.getInstance();
+//		ResourceEditor editor = new ResourceEditor();
+//		DataRepositoryCreator creator = new DataRepositoryCreator();
+//		DataGathering gatherer = new DataGathering();
+//
+//		List<IProject> projects = gatherer.getAllProjectAirdfiles();
+//		for(IProject eachProject : projects) {
+//			System.out.println("Project Name: "+eachProject.getName());
+//			System.out.println("Project Aird Path: "+gatherer.getAirdFile(eachProject));
+//		}
+//		
+//		System.out.println("Testing .monitorrepository return: "+ gatherer.getChosenFile(gatherer.getAllProjectAirdfiles().get(0), "monitorrepository"));
+//		
+//
+//		MonitorRepository monRep = start.getModelAccessor().getMonitorRepository().get(0);
+//		MeasuringPointRepository mpRep = start.getModelAccessor().getMeasuringPointRpository().get(0);
+//		Monitor mon1 = monRep.getMonitors().get(0);
+//		Monitor mon2 = monRep.getMonitors().get(1);
+//
+//		MeasuringPoint mp1 = mpRep.getMeasuringPoints().get(0);
+//		System.out.println("length: "+ mpRep.getMeasuringPoints().size());
+//		editor.addMeasuringPoint(mpRep, creator.createResourceURIMeasuringPoint("123gfsda"));
+//		System.out.println("length: "+ mpRep.getMeasuringPoints().size());
+//		for(MeasuringPoint mp : mpRep.getMeasuringPoints()) {
+//			System.out.println(mp.getResourceURIRepresentation());
+//		}
 
 		//		 
 		//		 System.out.println("Monitor: "+ mon1.getEntityName());
