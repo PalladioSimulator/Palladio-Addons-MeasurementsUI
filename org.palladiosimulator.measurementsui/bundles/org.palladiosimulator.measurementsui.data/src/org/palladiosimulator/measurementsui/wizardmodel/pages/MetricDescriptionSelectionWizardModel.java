dcf381a1f599894a80dbf2b714a1cd6852952786
David Schuetz <david-schuetz@gmx.de>
2018-09-11 15:24:27 +0200

    Completed Class

--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/wizardmodel/pages/MetricDescriptionSelectionWizardModel.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/wizardmodel/pages/MetricDescriptionSelectionWizardModel.java
@@ -1,30 +1,45 @@
 package org.palladiosimulator.measurementsui.wizardmodel.pages;
 
 import org.palladiosimulator.measurementsui.wizardmodel.WizardModel;
+import org.palladiosimulator.monitorrepository.Monitor;
 
 /**
  * 
  * @author David Schuetz
  *
  */
-public class MetricDescriptionSelectionWizardModel implements WizardModel{
+public class MetricDescriptionSelectionWizardModel implements WizardModel {
+	private Monitor monitor;
+	private static final String STANDARD_INFORMATION_MESSAGE = "Please select all Metrics which should be measured.";
+	private static final String NO_METRIC_SELECTED_MEASSAGE = "There is currently no Metric selected. "
+			+ "In order to get Simulation results you have select at least one Metric.";
+
+	public MetricDescriptionSelectionWizardModel(Monitor monitor) {
+		this.monitor = monitor;
+	}
 
 	@Override
 	public boolean canFinish() {
-		// TODO Auto-generated method stub
-		return false;
+		return !metricListIsEmpty();
 	}
 
 	@Override
 	public String getInfoText() {
-		// TODO Auto-generated method stub
-		return null;
+		if (metricListIsEmpty()) {
+			return NO_METRIC_SELECTED_MEASSAGE;
+		}
+		return STANDARD_INFORMATION_MESSAGE;
 	}
 
 	@Override
 	public void nextStep() {
 		// TODO Auto-generated method stub
-		
+	}
+	
+	public Monitor get
+	
+	private boolean metricListIsEmpty() {
+		return monitor.getMeasurementSpecifications().isEmpty();
 	}
 
 }
