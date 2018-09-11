df418aba9ec4227db9b6c0ee7a199a05816ec55c
nieuwefn <st111292@stud.uni-stuttgart.de>
2018-09-11 21:39:00 +0200

    added javadoc

--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/datamanipulation/ResourceEditor.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/datamanipulation/ResourceEditor.java
@@ -6,75 +6,78 @@ import org.palladiosimulator.metricspec.MetricDescription;
 import org.palladiosimulator.monitorrepository.MeasurementSpecification;
 
 public interface ResourceEditor {
-    /**
-     * Changes the EntityName of a resource
-     * 
-     * @param resource
-     *            The resource to change the name
-     * @param newName
-     *            The new name of the resource
-     */
-    void setResourceName(EObject resource, String newName);
+	/**
+	 * Changes the EntityName of a resource
+	 * 
+	 * @param resource The resource to change the name
+	 * @param newName  The new name of the resource
+	 */
+	void setResourceName(EObject resource, String newName);
 
-    /**
-     * Changes the current status of "activated" to its negative
-     * 
-     * @param monitor
-     *            the monitor to change
-     * @param currentBool
-     *            the value to change
-     */
-    void changeMonitorActive(EObject monitor, boolean currentBool);
+	/**
+	 * Changes the current status of "activated" to its negative
+	 * 
+	 * @param monitor     the monitor to change
+	 * @param currentBool the value to change
+	 */
+	void changeMonitorActive(EObject monitor, boolean currentBool);
 
-    /**
-     * Changes current bool triggers self adapting to its negative
-     * 
-     * @param mspec
-     *            the measurement specification to change
-     * @param currentBool
-     *            current value of triggersSelfAdaptions
-     */
-    void changeTriggersSelfAdapting(MeasurementSpecification mspec, boolean currentBool);
+	/**
+	 * Changes current bool triggers self adapting to its negative
+	 * 
+	 * @param mspec       the measurement specification to change
+	 * @param currentBool current value of triggersSelfAdaptions
+	 */
+	void changeTriggersSelfAdapting(MeasurementSpecification mspec, boolean currentBool);
 
-    /**
-     * alternative method to set measuring points, if it is not possible with parsley
-     * 
-     * @param monitor
-     * @param mp
-     */
-    void setMeasuringPoint(EObject monitor, MeasuringPoint mp);
+	/**
+	 * alternative method to set measuring points, if it is not possible with
+	 * parsley
+	 * 
+	 * @param monitor
+	 * @param mp
+	 */
+	void setMeasuringPoint(EObject monitor, MeasuringPoint mp);
 
-    /**
-     * alternative method to add measuring points, if it is not possible with parsley
-     * 
-     * @param mpRep
-     * @param mp
-     */
-    void addMeasuringPoint(EObject mpRep, EObject mp);
+	/**
+	 * alternative method to add measuring points, if it is not possible with
+	 * parsley
+	 * 
+	 * @param mpRep
+	 * @param mp
+	 */
+	void addMeasuringPoint(EObject mpRep, EObject mp);
 
-    /**
-     * Delete the passed EObject
-     * 
-     * @param objToDelete
-     */
-    void deleteResource(EObject objToDelete);
+	/**
+	 * Delete the passed EObject
+	 * 
+	 * @param objToDelete
+	 */
+	void deleteResource(EObject objToDelete);
 
-    /**
-     * Sets MetricDescription for a MeasurementSpecification. A List of all Metric Descriptions can
-     * be gotten through: aMSpec.getMetricDescription().getRepository().getMetricDescriptions()
-     * 
-     * @param aMeasurementSpecification
-     * @param aMetricDescription
-     */
-    void setMetricDescription(EObject aMeasurementSpecification, MetricDescription aMetricDescription);
+	/**
+	 * Sets MetricDescription for a MeasurementSpecification. A List of all Metric
+	 * Descriptions can be gotten through:
+	 * aMSpec.getMetricDescription().getRepository().getMetricDescriptions()
+	 * 
+	 * @param aMeasurementSpecification
+	 * @param aMetricDescription
+	 */
+	void setMetricDescription(EObject aMeasurementSpecification, MetricDescription aMetricDescription);
 
-    /**
-     * Adds a new MeasurementSpecification to the list of MeasurementSpecifications for the monitor
-     * 
-     * @param monitor
-     *            The Monitor to which the new specification is added
-     */
-    void addMeasurementSpecification(EObject monitor);
+	/**
+	 * Adds a new MeasurementSpecification to the list of MeasurementSpecifications
+	 * for the monitor
+	 * 
+	 * @param monitor The Monitor to which the new specification is added
+	 */
+	void addMeasurementSpecification(EObject monitor);
 
-    void addMonitorToRepository(EObject monitorRepository, EObject monitor);
+	/**
+	 * adds a Monitor to a Monitor Repository through AddCommands
+	 * 
+	 * @param monitorRepository
+	 * @param monitor
+	 */
+	void addMonitorToRepository(EObject monitorRepository, EObject monitor);
 }
