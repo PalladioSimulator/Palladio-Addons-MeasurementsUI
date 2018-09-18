42708b69760d48a7776acdb5148424a6ed5e6800
enpro1 <domasm@live.de>
2018-09-18 16:51:25 +0200

    Added icons to models

--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizardmain/handlers/MeasuringPointsLabelProvider.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizardmain/handlers/MeasuringPointsLabelProvider.java
@@ -3,17 +3,34 @@ package org.palladiosimulator.measurementsui.wizardmain.handlers;
 import java.util.LinkedList;
 
 import org.eclipse.emf.common.EMFPlugin;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
-import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
-import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.swt.graphics.Image;
+import org.palladiosimulator.pcm.core.composition.AssemblyContext;
+import org.palladiosimulator.pcm.core.composition.provider.AssemblyContextItemProvider;
 import org.palladiosimulator.pcm.core.entity.NamedElement;
+import org.palladiosimulator.pcm.provider.PcmItemProviderAdapterFactory;
+import org.palladiosimulator.pcm.resourceenvironment.LinkingResource;
 import org.palladiosimulator.pcm.resourceenvironment.ProcessingResourceSpecification;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
+import org.palladiosimulator.pcm.resourceenvironment.provider.LinkingResourceItemProvider;
+import org.palladiosimulator.pcm.resourceenvironment.provider.ResourceContainerItemProvider;
+import org.palladiosimulator.pcm.resourceenvironment.provider.ResourceEnvironmentItemProvider;
+import org.palladiosimulator.pcm.resourcetype.provider.ProcessingResourceTypeItemProvider;
+import org.palladiosimulator.pcm.seff.ExternalCallAction;
+import org.palladiosimulator.pcm.seff.provider.ExternalCallActionItemProvider;
+import org.palladiosimulator.pcm.subsystem.SubSystem;
+import org.palladiosimulator.pcm.subsystem.provider.SubSystemItemProvider;
+import org.palladiosimulator.pcm.system.System;
+import org.palladiosimulator.pcm.system.provider.SystemItemProvider;
+import org.palladiosimulator.pcm.usagemodel.EntryLevelSystemCall;
+import org.palladiosimulator.pcm.usagemodel.UsageScenario;
+import org.palladiosimulator.pcm.usagemodel.provider.EntryLevelSystemCallItemProvider;
+import org.palladiosimulator.pcm.usagemodel.provider.UsageModelItemProvider;
 
 /**
  * 
@@ -21,73 +38,98 @@ import org.palladiosimulator.pcm.resourceenvironment.ProcessingResourceSpecifica
  *
  */
 public class MeasuringPointsLabelProvider implements ILabelProvider {
-	EMFPlugin plug = new EcoreEditPlugin();
+    EMFPlugin plug = new EcoreEditPlugin();
 
-	@Override
-	public void addListener(ILabelProviderListener listener) {
-		// TODO Auto-generated method stub
+    @Override
+    public void addListener(ILabelProviderListener listener) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public boolean isLabelProperty(Object element, String property) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean isLabelProperty(Object element, String property) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void removeListener(ILabelProviderListener listener) {
-		// TODO Auto-generated method stub
+    @Override
+    public void removeListener(ILabelProviderListener listener) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public Image getImage(Object element) {
-		if (element instanceof LinkedList) {
-			return null;
-		} else {
-			EObject object = (EObject) element;
-			EcoreItemProviderAdapterFactory factory = new EcoreItemProviderAdapterFactory();
-			if (factory.isFactoryForType(IItemLabelProvider.class)) {
-				IItemLabelProvider labelProvider = (IItemLabelProvider) factory.adapt(object, IItemLabelProvider.class);
-				if (labelProvider != null) {
-					URI test = (URI) labelProvider.getImage(object);
+    @Override
+    public Image getImage(Object element) {
+        PcmItemProviderAdapterFactory factory = new PcmItemProviderAdapterFactory();
+        EObject object = (EObject) element;
+        if (element instanceof LinkedList) {
+            return null;
+        } else {
+            if (object instanceof UsageScenario) {
+                UsageModelItemProvider mod = new UsageModelItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof ResourceEnvironment) {
+                ResourceEnvironmentItemProvider mod = new ResourceEnvironmentItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
 
-					return ExtendedImageRegistry.getInstance().getImage(test);
-				}
-			}
-		}
+            } else if (object instanceof System) {
+                SystemItemProvider mod = new SystemItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof AssemblyContext) {
+                AssemblyContextItemProvider mod = new AssemblyContextItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof ResourceContainer) {
+                ResourceContainerItemProvider mod = new ResourceContainerItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof ProcessingResourceSpecification) {
+                ProcessingResourceTypeItemProvider mod = new ProcessingResourceTypeItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof LinkingResource) {
+                LinkingResourceItemProvider mod = new LinkingResourceItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof ExternalCallAction) {
+                ExternalCallActionItemProvider mod = new ExternalCallActionItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof EntryLevelSystemCall) {
+                EntryLevelSystemCallItemProvider mod = new EntryLevelSystemCallItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            } else if (object instanceof SubSystem) {
+                SubSystemItemProvider mod = new SubSystemItemProvider(factory);
+                return ExtendedImageRegistry.getInstance().getImage(mod.getImage(object));
+            }
 
-		return null;
-	}
+        }
 
-	@Override
-	public String getText(Object element) {
-		if (element instanceof LinkedList) {
-			if (!((LinkedList) element).isEmpty()) {
-				return (((LinkedList) element).get(0).getClass().getSimpleName().replaceAll("Impl", "")
-						.replaceAll("([A-Z])", " $1"));
-			} else {
-				return null;
-			}
+        return null;
+    }
 
-			// return "Liste";
-		} else {
-			if (element instanceof ProcessingResourceSpecification) {
-				return ((ProcessingResourceSpecification) element).getActiveResourceType_ActiveResourceSpecification()
-						.getEntityName();
+    @Override
+    public String getText(Object element) {
+        if (element instanceof LinkedList) {
+            if (!((LinkedList) element).isEmpty()) {
+                return (((LinkedList) element).get(0).getClass().getSimpleName().replaceAll("Impl", "")
+                        .replaceAll("([A-Z])", " $1"));
+            } else {
+                return null;
+            }
 
-			} else {
-				return ((NamedElement) element).getEntityName();
-			}
-		}
+            // return "Liste";
+        } else {
+            if (element instanceof ProcessingResourceSpecification) {
+                return ((ProcessingResourceSpecification) element).getActiveResourceType_ActiveResourceSpecification()
+                        .getEntityName();
 
-	}
+            } else {
+                return ((NamedElement) element).getEntityName();
+            }
+        }
+
+    }
 
 }
