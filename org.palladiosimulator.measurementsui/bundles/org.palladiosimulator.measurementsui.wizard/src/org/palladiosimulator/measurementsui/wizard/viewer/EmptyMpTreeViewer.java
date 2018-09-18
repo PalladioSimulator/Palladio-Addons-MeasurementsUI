adee32418bb85fee782670dec79269d735cf315d
David Schuetz <david-schuetz@gmx.de>
2018-09-18 11:50:58 +0200

    Refactored Wizard Viewer

--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizard/viewer/EmptyMpTreeViewer.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizard/viewer/EmptyMpTreeViewer.java
@@ -7,33 +7,34 @@ import org.palladiosimulator.measurementsui.dataprovider.DataApplication;
 
 import emptymeasuringpoints.EmptymeasuringpointsInjectorProvider;
 
-
 /**
- * Generates a tree view where all empty measuring points from all
- * measuringpoint repositories in the workspace are shown.
+ * Generates a tree view where all empty measuring points from all measuringpoint repositories in
+ * the workspace are shown.
  * 
  * @author David Schuetz
  */
 public class EmptyMpTreeViewer extends WizardTreeViewer {
 
-	/**
-	 * 
-	 * @param parent         container where the tree viewer is placed in
-	 * @param application    Connection to the data binding. This is needed in order
-	 *                       to get the repository of the current project.
-	 */
-	public EmptyMpTreeViewer(Composite parent, DataApplication application) {
-		super(parent, application);
-	}
+    /**
+     * 
+     * @param parent
+     *            the container where the tree viewer is placed in
+     * @param application
+     *            the connection to the data binding. This is needed in order to get the repository
+     *            of the current project.
+     */
+    public EmptyMpTreeViewer(Composite parent, DataApplication application) {
+        super(parent, application);
+    }
 
-	@Override
-	protected EObject getModelRepository() {
-		return dataApplication.getModelAccessor().getMeasuringPointRepository().get(0);
-	}
+    @Override
+    protected EObject getModelRepository() {
+        return dataApplication.getModelAccessor().getMeasuringPointRepository().get(0);
+    }
 
-	@Override
-	protected void initInjector() {
-		injector = EmptymeasuringpointsInjectorProvider.getInjector();
-	}
+    @Override
+    protected void initInjector() {
+        injector = EmptymeasuringpointsInjectorProvider.getInjector();
+    }
 
 }
--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizard/viewer/SelectMeasurementsViewer.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.wizard/src/org/palladiosimulator/measurementsui/wizard/viewer/SelectMeasurementsViewer.java
@@ -9,29 +9,32 @@ import tableform.TableformInjectorProvider;
 
 /**
  * Generates a table view where all MeasurementSpecifications of a monitor are shown
+ * 
  * @author David Schuetz
  *
  */
 public class SelectMeasurementsViewer extends WizardTableViewer {
-	/**
-	 * 
-	 * @param parent          container where the tree viewer is placed in
-	 * @param dataApplication Connection to the data binding. This is needed in
-	 *                        order to get the repository of the current project.
-	 */
-	public SelectMeasurementsViewer(Composite parent, WizardModel wizardModel) {
-		super(parent, wizardModel);
-	}
+    /**
+     * 
+     * @param parent
+     *            the container where the tree viewer is placed in
+     * @param dataApplication
+     *            the connection to the data binding. This is needed in order to get the repository
+     *            of the current project.
+     */
+    public SelectMeasurementsViewer(Composite parent, WizardModel wizardModel) {
+        super(parent, wizardModel);
+    }
 
-	@Override
-	protected void initInjector() {
-		this.injector = TableformInjectorProvider.getInjector();
-	}
+    @Override
+    protected void initInjector() {
+        this.injector = TableformInjectorProvider.getInjector();
+    }
 
-	@Override
-	protected EObject getModelRepository() {
-		MetricDescriptionSelectionWizardModel model = (MetricDescriptionSelectionWizardModel) wizardModel;
-		return model.getUnusedMetricsMonitor();
-	}
+    @Override
+    protected EObject getModelRepository() {
+        MetricDescriptionSelectionWizardModel model = (MetricDescriptionSelectionWizardModel) wizardModel;
+        return model.getUnusedMetricsMonitor();
+    }
 
 }
