917ac5886467a0ebe74172c0e70e72e9d9f758e5
Florian <st111292@stud.uni-stuttgart.de>
2018-09-20 16:27:27 +0200

    Data Provider and Wizard Model for the 4th page.  PALEP18-131

---
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/dataprovider/ProcessingTypeProvider.java
@@ -0,0 +1,161 @@
+package org.palladiosimulator.measurementsui.dataprovider;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.palladiosimulator.monitorrepository.MeasurementSpecification;
+import org.palladiosimulator.monitorrepository.MonitorRepositoryFactory;
+import org.palladiosimulator.monitorrepository.MonitorRepositoryPackage;
+import org.palladiosimulator.monitorrepository.ProcessingType;
+import org.palladiosimulator.monitorrepository.impl.FeedThroughImpl;
+import org.palladiosimulator.monitorrepository.impl.FixedSizeAggregationImpl;
+import org.palladiosimulator.monitorrepository.impl.TimeDrivenAggregationImpl;
+import org.palladiosimulator.monitorrepository.impl.TimeDrivenImpl;
+import org.palladiosimulator.monitorrepository.impl.VariableSizeAggregationImpl;
+
+public class ProcessingTypeProvider {
+
+    private static final String EDIT_MODE_NOT_YET_SUPPORTERD = "Edit Mode not yet supporterd";
+    MonitorRepositoryFactory monFactory = MonitorRepositoryPackage.eINSTANCE.getMonitorRepositoryFactory();
+
+    public String[] provideBasicProcessingTypes() {
+        EList<EObject> listOfProcessingTypes = new BasicEList<>();
+        listOfProcessingTypes.add(monFactory.createFeedThrough());
+        listOfProcessingTypes.add(monFactory.createFixedSizeAggregation());
+        listOfProcessingTypes.add(monFactory.createTimeDriven());
+        listOfProcessingTypes.add(monFactory.createTimeDrivenAggregation());
+        listOfProcessingTypes.add(monFactory.createVariableSizeAggregation());
+        String[] allProcessingTypesArray = new String[5];
+
+        for (EObject aProcessingType : listOfProcessingTypes) {
+            if (aProcessingType instanceof FeedThroughImpl) {
+                allProcessingTypesArray[0] = "FeedThrough";
+
+            } else if (aProcessingType instanceof FixedSizeAggregationImpl) {
+                allProcessingTypesArray[1] = "FixedSizeAggregation";
+
+            } else if (aProcessingType instanceof TimeDrivenImpl) {
+                if (aProcessingType instanceof TimeDrivenAggregationImpl) {
+                    allProcessingTypesArray[3] = "TimeDrivenAggregation";
+
+                } else {
+                    allProcessingTypesArray[2] = "TimeDriven";
+                }
+
+            } else if (aProcessingType instanceof VariableSizeAggregationImpl) {
+                allProcessingTypesArray[4] = "VariableSizeAggregation";
+
+            }
+
+        }
+        return allProcessingTypesArray;
+    }
+
+    public List<String> provideProcessingTypeProperties(String processingTypeString) {
+        LinkedList<String> propertiesForProcessingType = new LinkedList<>();
+        switch (processingTypeString) {
+        case "FeedThrough":
+            break;
+        case "FixedSizeAggregation":
+            propertiesForProcessingType.add("Frequency");
+            propertiesForProcessingType.add("Number of Measurements");
+            break;
+        case "TimeDriven":
+        case "TimeDrivenAggregation":
+            propertiesForProcessingType.add("Window Increment");
+            propertiesForProcessingType.add("Window Length");
+            break;
+        case "VariableSizeAggregation":
+            propertiesForProcessingType.add("Frequency");
+            propertiesForProcessingType.add("Retrospection Length");
+            break;
+        }
+        return propertiesForProcessingType;
+    }
+
+    public void assignProcessingTypeToMeasurementSpecification(MeasurementSpecification measurementSpecification,
+            String selectedProcessingType, boolean isEditing) {
+
+        switch (selectedProcessingType) {
+        case "FeedThrough":
+            if (isEditing) {
+                System.err.println(EDIT_MODE_NOT_YET_SUPPORTERD);
+            } else {
+                measurementSpecification.setProcessingType(monFactory.createFeedThrough());
+            }
+
+            break;
+        case "FixedSizeAggregation":
+            if (isEditing) {
+                System.err.println(EDIT_MODE_NOT_YET_SUPPORTERD);
+            } else {
+                measurementSpecification.setProcessingType(monFactory.createFixedSizeAggregation());
+            }
+            break;
+        case "TimeDriven":
+            if (isEditing) {
+                System.err.println(EDIT_MODE_NOT_YET_SUPPORTERD);
+            } else {
+                measurementSpecification.setProcessingType(monFactory.createTimeDriven());
+            }
+            break;
+        case "TimeDrivenAggregation":
+            if (isEditing) {
+                System.err.println(EDIT_MODE_NOT_YET_SUPPORTERD);
+            } else {
+                measurementSpecification.setProcessingType(monFactory.createTimeDrivenAggregation());
+            }
+            break;
+        case "VariableSizeAggregation":
+            if (isEditing) {
+                System.err.println(EDIT_MODE_NOT_YET_SUPPORTERD);
+            } else {
+                measurementSpecification.setProcessingType(monFactory.createVariableSizeAggregation());
+            }
+            break;
+        default:
+            System.err.println("You done fucked up boy");
+        }
+
+    }
+
+    public void setProcessingTypeAttributes(MeasurementSpecification measurementSpecification, List<Double> values,
+            boolean isEditing) {
+        ProcessingType processingType = measurementSpecification.getProcessingType();
+        if (processingType instanceof FeedThroughImpl) {
+
+        } else if (processingType instanceof FixedSizeAggregationImpl) {
+            if (isEditing) {
+
+            } else {
+                ((FixedSizeAggregationImpl) processingType).setFrequency(values.get(0).intValue());
+                ((FixedSizeAggregationImpl) processingType).setNumberOfMeasurements(values.get(1).intValue());
+            }
+
+        } else if (processingType instanceof TimeDrivenImpl) { // works for both Time Driven and
+                                                               // TimeDrivenAggregation, since
+                                                               // Aggregation is child of TimeDriven
+                                                               // and both require the same values
+                                                               // to be set
+            if (isEditing) {
+
+            } else {
+                ((TimeDrivenImpl) processingType).setWindowIncrement(values.get(0));
+                ((TimeDrivenImpl) processingType).setWindowLength(values.get(1));
+            }
+
+        } else if (processingType instanceof VariableSizeAggregationImpl) {
+            if (isEditing) {
+
+            } else {
+                ((VariableSizeAggregationImpl) processingType).setFrequency(values.get(0).intValue());
+                ((VariableSizeAggregationImpl) processingType).setRetrospectionLength(values.get(1).intValue());
+            }
+
+        }
+
+    }
+}
--- org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/wizardmodel/pages/ProcessingTypeSelectionWizardModel.java
+++ org.palladiosimulator.measurementsui/bundles/org.palladiosimulator.measurementsui.data/src/org/palladiosimulator/measurementsui/wizardmodel/pages/ProcessingTypeSelectionWizardModel.java
@@ -1,32 +1,99 @@
 package org.palladiosimulator.measurementsui.wizardmodel.pages;
 
+import java.util.List;
+
+import org.palladiosimulator.measurementsui.dataprovider.ProcessingTypeProvider;
 import org.palladiosimulator.measurementsui.wizardmodel.WizardModel;
+import org.palladiosimulator.monitorrepository.MeasurementSpecification;
+import org.palladiosimulator.monitorrepository.Monitor;
 
 public class ProcessingTypeSelectionWizardModel implements WizardModel {
 
-	@Override
-	public boolean canFinish() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    private static final String INFORMATION_MESSAGE = "Please select a Processing Type for each Metric Description";
+    private static final String PROCESSING_TYPE_TITLE = "Select Processing Types";
 
-	@Override
-	public String getInfoText() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private Monitor usedMetricsMonitor;
+    private boolean isEditing;
+    private ProcessingTypeProvider provider;
 
-	@Override
-	public boolean nextStep() {
-		// TODO Auto-generated method stub
-	    return true;
+    /**
+     * 
+     * @param monitor
+     *            the monitor containing the selected MeasurementSpecifications from Page 3
+     * @param isEditing
+     *            Boolean containing information if we are in edit mode or not
+     */
+    public ProcessingTypeSelectionWizardModel(Monitor monitor, boolean isEditing) {
+        this.usedMetricsMonitor = monitor;
+        this.isEditing = isEditing;
+        this.provider = new ProcessingTypeProvider();
+    }
 
-	}
+    @Override
+    public boolean canFinish() {
+        // TODO Auto-generated method stub
+        return true;
+    }
 
-	@Override
-	public String getTitleText() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getInfoText() {
+        return INFORMATION_MESSAGE;
+    }
+
+    @Override
+    public boolean nextStep() {
+        return false;
+
+    }
+
+    @Override
+    public String getTitleText() {
+        return PROCESSING_TYPE_TITLE;
+    }
+
+    /**
+     * Provides all possible Processing Types in a String Array, used to fill the dropdown in the
+     * 4th page.
+     * 
+     * @return A String Array containing all possible processsing types
+     */
+    public String[] providePossibleProcessingTypes() {
+        return provider.provideBasicProcessingTypes();
+    }
+
+    /**
+     * assigns the Processing Type depending on which was selected in the dropdown.
+     * 
+     * @param aMeasurementSpecification
+     * @param selectedProcessingType
+     */
+    public void assignProcessingType(MeasurementSpecification aMeasurementSpecification,
+            String selectedProcessingType) {
+        provider.assignProcessingTypeToMeasurementSpecification(aMeasurementSpecification, selectedProcessingType,
+                isEditing);
+    }
+
+    /**
+     * Returns a list of String for the modification fields of the corresponding Processing Type.
+     * Used for the 3rd and 4th columns in the 4th page.
+     * 
+     * @param processingTypeString
+     * @return
+     */
+    public List<String> fieldsForThisProcessingType(String processingTypeString) {
+        return provider.provideProcessingTypeProperties(processingTypeString);
+    }
+
+    /**
+     * Sets the values for the modification fields of each Processing Type.
+     * 
+     * @param aMeasurementSpecification
+     * @param floatValues
+     */
+    public void editProcessingTypeAttributes(MeasurementSpecification aMeasurementSpecification,
+            List<Double> floatValues) {
+        provider.setProcessingTypeAttributes(aMeasurementSpecification, floatValues, isEditing);
+
+    }
 
 }
