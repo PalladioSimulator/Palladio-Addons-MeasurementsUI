761fea9c28ca826aec0960b1a4bae05d7f621294
mehme <mehme@DESKTOP-AIPKUFE>
2018-10-14 11:31:29 +0200

    Mockito Test fot Measuring Point Selection Wizard Model.

---
+++ org.palladiosimulator.measurementsui/tests/JUnitMockito.it.tests/src/org/palladio/simulator/junit/mockito/tests/MeasuringPointSelectionWizardModelTest.java
@@ -0,0 +1,118 @@
+package org.palladio.simulator.junit.mockito.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.stubbing.Answer;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPointRepository;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringpointFactory;
+import org.palladiosimulator.measurementsui.datamanipulation.DataEditor;
+import org.palladiosimulator.measurementsui.datamanipulation.ResourceEditorImpl;
+import org.palladiosimulator.measurementsui.dataprovider.DataApplication;
+import org.palladiosimulator.measurementsui.wizardmodel.pages.MeasuringPointSelectionWizardModel;
+import org.palladiosimulator.monitorrepository.Monitor;
+import org.palladiosimulator.monitorrepository.MonitorRepositoryFactory;
+import org.palladiosimulator.pcm.core.composition.AssemblyContext;
+import org.palladiosimulator.pcm.resourceenvironment.LinkingResource;
+import org.palladiosimulator.pcm.resourceenvironment.ProcessingResourceSpecification;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+import org.palladiosimulator.pcm.usagemodel.UsageScenario;
+import org.palladiosimulator.pcmmeasuringpoint.PcmmeasuringpointFactory;
+import org.palladiosimulator.pcmmeasuringpoint.PcmmeasuringpointPackage;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Matchers.*;
+
+public class MeasuringPointSelectionWizardModelTest {
+	@Mock
+	private Monitor monitor;
+	private boolean isEditing;
+	private boolean finishable = true;
+	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
+	@Test
+	public void testIsFinishable() {
+		MeasuringPointSelectionWizardModel measuringPointSelectionMock = Mockito.mock(MeasuringPointSelectionWizardModel.class);
+		Boolean finishable = measuringPointSelectionMock.isFinishable();
+		when(measuringPointSelectionMock.isFinishable()).thenReturn(finishable);
+		//assertTrue(true);
+}
+	@Test
+	public void testSetFinishable() {
+		MeasuringPointSelectionWizardModel measuringPointSelectionMock = Mockito.mock(MeasuringPointSelectionWizardModel.class);
+		Mockito.doCallRealMethod().when(measuringPointSelectionMock).setFinishable(true);
+			
+}
+	@Test
+	public void testIsEditing() {
+		MeasuringPointSelectionWizardModel measuringPointSelectionMock = Mockito.mock(MeasuringPointSelectionWizardModel.class);
+		Boolean editable = measuringPointSelectionMock.isEditing();
+		when(measuringPointSelectionMock.isFinishable()).thenReturn(isEditing);
+		
+}
+	@Test
+	public void testSetEditing() {
+		MeasuringPointSelectionWizardModel setEditingMock = Mockito.mock(MeasuringPointSelectionWizardModel.class);
+		Mockito.doCallRealMethod().when(setEditingMock).setEditing(isEditing);
+			
+}
+	@Test
+	public void testCanFinish() {
+		MeasuringPointSelectionWizardModel measuringPointSelectionMock = mock(MeasuringPointSelectionWizardModel.class);
+		Monitor monitor = MonitorRepositoryFactory.eINSTANCE.createMonitor();
+		boolean finishable =  measuringPointSelectionMock.canFinish();
+		when(monitor.getMeasuringPoint() != null).thenReturn(finishable);
+		//assertTrue(true);
+}
+	@Test
+	public void testGetInfoText() {
+		MeasuringPointSelectionWizardModel measuringPointSelectionMock = mock(MeasuringPointSelectionWizardModel.class);
+		Monitor monitor = MonitorRepositoryFactory.eINSTANCE.createMonitor();
+		String infoText = measuringPointSelectionMock.getInfoText();
+		
+		when(measuringPointSelectionMock.getInfoText()).thenAnswer(new Answer() {
+			private static final String CREATE_MEASURINGPOINT_INFO_TEXT = "Select the element of your Models which should be "
+					+ "monitored during a simulation run. Models for which a measuring point can be created are highlighted with a bold font.";
+			private static final String EDIT_MEASURINGPOINT_INFO_TEXT = "Select a different measuring Point.";       
+
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				if(monitor.getMonitorRepository() != null) 
+					return EDIT_MEASURINGPOINT_INFO_TEXT;
+				
+				return CREATE_MEASURINGPOINT_INFO_TEXT;
+			}
+			
+		});
+}
+	@Test
+	public void testGetTitleText() {
+		MeasuringPointSelectionWizardModel measuringPointSelectionMock = mock(MeasuringPointSelectionWizardModel.class);
+		Monitor monitor = MonitorRepositoryFactory.eINSTANCE.createMonitor();
+		String titleText = measuringPointSelectionMock.getInfoText();
+		when(measuringPointSelectionMock.getInfoText()).thenAnswer(new Answer() {
+			private static final String CREATE_MEASURINGPOINT_TITLE = "Create Measuring Point";
+			private static final String EDIT_MEASURINGPOINT_TITLE = "Edit Measuring Point";
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				if(monitor.getMeasuringPoint() != null) 
+					return CREATE_MEASURINGPOINT_TITLE;
+				
+				return EDIT_MEASURINGPOINT_TITLE;
+			}
+			
+		});
+}
+	
+	
+
+
+
+}
